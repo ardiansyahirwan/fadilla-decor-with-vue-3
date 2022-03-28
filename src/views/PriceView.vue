5a4738ef34dc6d52a0b56a1b1df70b09463f821d
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2022-03-28 13:51:49 +0700

    pricelist: add tab navigation

--- src/views/PriceView.vue
+++ src/views/PriceView.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="price">
-    <h1>This is an price page</h1>
+    <div class="tab-selection">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="3"
+            align-self="center"
+            class="text-center text-md-left"
+          >
+            <v-text class="font-weight-bold text-md-h4">
+              <span
+                >Our <br />
+                Partnership
+              </span>
+            </v-text>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
   </div>
 </template>
