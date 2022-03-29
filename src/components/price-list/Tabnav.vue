4665a63d8e33e3519b82bc59cc2645ec892c521c
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2022-03-29 13:24:41 +0700

    price-list: modify tab Nav add item All Package

--- src/components/price-list/Tabnav.vue
+++ src/components/price-list/Tabnav.vue
@@ -1,39 +1,42 @@
 <template>
-  <v-row>
-    <v-col cols="4" md="3" align-self="center">
-      <v-text>
-        <slot name="tab-title">Default Content</slot>
-      </v-text>
-    </v-col>
+  <v-container>
+    <v-row>
+      <v-col cols="6" sm="4" md="2" align-self="center" class="text-left">
+        <v-text>
+          <slot name="tab-title">Default Content</slot>
+        </v-text>
+      </v-col>
 
-    <v-col
-      md="2"
-      align-self="center"
-      v-for="nav in tabNavigation"
-      :key="nav.id"
-      class="ml-md-3"
-    >
-      <v-card
-        rounded="xl"
-        class="text-center"
-        :class="backgroundForTabnav(nav.id)"
+      <v-col
+        cols="6"
+        sm="4"
+        md="2"
+        align-self="center"
+        v-for="nav in tabNavigation"
+        :key="nav.id"
       >
-        <v-card-content>
-          <v-icon
-            size="60"
-            icon="mdi-heart-outline"
-            color="orange-lighten-1"
-          ></v-icon>
-          <v-text>
-            <p class="text-brown-color">Order By</p>
-            <h5 class="text-md-subtitle-1 font-weight-bold text-brown-color">
-              {{ nav.name }}
-            </h5>
-          </v-text>
-        </v-card-content>
-      </v-card>
-    </v-col>
-  </v-row>
+        <v-card
+          rounded="xl"
+          class="text-center"
+          :class="backgroundForTabnav(nav.id)"
+        >
+          <v-card-content>
+            <v-icon
+              size="60"
+              icon="mdi-heart-outline"
+              color="orange-lighten-1"
+            ></v-icon>
+            <v-text>
+              <p class="text-brown-color">Order By</p>
+              <h5 class="text-md-subtitle-1 font-weight-bold text-brown-color">
+                {{ nav.name }}
+              </h5>
+            </v-text>
+          </v-card-content>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -42,10 +45,11 @@ export default {
   methods: {
     backgroundForTabnav(id) {
       return {
-        "bg-orange-lighten-4": id == 1,
-        "bg-yellow-lighten-3": id == 2,
-        "bg-lime-lighten-3": id == 3,
-        "bg-green-accent-1": id == 4,
+        "bg-deep-orange-lighten-4": id == 1,
+        "bg-orange-lighten-4": id == 2,
+        "bg-yellow-lighten-3": id == 3,
+        "bg-lime-lighten-3": id == 4,
+        "bg-green-accent-1": id == 5,
       };
     },
   },
--- src/views/PriceView.vue
+++ src/views/PriceView.vue
@@ -1,36 +1,39 @@
 <template>
   <div class="price">
     <div class="tab-selection mt-md-10">
-      <v-container>
-        <Tabnav :tabNavigation="tabNav.itemsNav">
-          <template #tab-title>
-            <span
-              class="font-weight-bold text-md-h4"
-              v-html="tabNav.title"
-            ></span>
-            <p class="text-md-subtitle-1" v-html="tabNav.subtitleText"></p
-          ></template>
-        </Tabnav>
-      </v-container>
+      <Tabnav :tabNavigation="tabNav.itemsNav">
+        <template #tab-title>
+          <span
+            class="font-weight-bold text-md-h4"
+            v-html="tabNav.title"
+          ></span>
+          <p class="text-md-subtitle-1" v-html="tabNav.subtitleText"></p
+        ></template>
+      </Tabnav>
     </div>
+
+    <Footer />
   </div>
 </template>
 
 <script>
 import Tabnav from "@/components/price-list/Tabnav.vue";
+import Footer from "@/components/FooterComponent.vue";
 export default {
   components: {
     Tabnav,
+    Footer,
   },
   data: () => ({
     tabNav: {
-      title: "Our <br> Price List",
-      subtitleText: "Search your design <br> decoration here",
+      title: "Our Price List",
+      subtitleText: "Search your design decoration here",
       itemsNav: [
-        { id: 1, name: "Intimate Wedding" },
-        { id: 2, name: "Akad" },
-        { id: 3, name: "Garden Party" },
-        { id: 4, name: "Resepsi All-in" },
+        { id: 1, name: "All Package" },
+        { id: 2, name: "Intimate Wedding" },
+        { id: 3, name: "Akad" },
+        { id: 4, name: "Garden Party" },
+        { id: 5, name: "Resepsi All-in" },
       ],
     },
   }),
